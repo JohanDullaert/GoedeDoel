1b6a43534ab8c2ae3ba58fa27af59fb577c5af60
Johan Dullaert <johan.dullaert@vdab.be>
2017-08-10 13:25:57 +0200

    updates for javadoc on Jenkins

--- src/main/java/be/vdab/entities/GoedeDoel.java
+++ src/main/java/be/vdab/entities/GoedeDoel.java
@@ -2,18 +2,38 @@ package be.vdab.entities;
 
 import java.math.BigDecimal;
 
+/**
+ * Een <strong>goed doel</strong> waarvoor men geld inzamelt
+ * @author Johan.Dullaert
+ *
+ */
 public class GoedeDoel {
 	private final String naam;
 	private BigDecimal opgebracht = BigDecimal.ZERO;
 
+	/**
+	 * Maakt een GoedeDoel object 
+	 * en dit is een voorbeeld van slechte javadoc want dit is een constructor in deze class, 
+	 * wat zou die anders doen?
+	 * 
+	 * @param naam De naam van het goede doel
+	 */
 	public GoedeDoel(String naam) {
 		this.naam = naam;
 	}
 
+	/**
+	 * geeft de naam terug, jeeesus...
+	 * @return de naam
+	 */
 	public String getNaam() {
 		return naam;
 	}
 
+	/**
+	* Geeft terug hoeveel &euro; dit doel heeft opgebracht
+	* @return het opgebracht bedrag
+	*/
 	public BigDecimal getOpgebracht() {
 		return opgebracht;
 	}
